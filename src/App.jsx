20ea43f673e7fce56f2862851a69fdaf2b4c9d03
Piotr Juszczak <piort789@gmail.com>
2023-12-29 02:43:16 +0100

    Add editing personal information

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ import "./styles/normalize.css";
 import "./styles/App.css";
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    firstName: "",
+    lastName: "",
+    position: "",
+    email: "",
+    phoneNumber: "",
+    adress: "",
+    summary: "",
+  });
+
+  function handlePersonalInfoChange(e) {
+    const fieldValue = e.target.value;
+    const fieldName = e.target.name;
+
+    setPersonalInfo((prevPersonalInfo) => {
+      return { ...prevPersonalInfo, [fieldName]: fieldValue };
+    });
+  }
+
   return (
     <>
       <aside className="aside-container">
@@ -19,10 +38,21 @@ function App() {
           </h1>
         </header>
 
-        <PersonalInfoForm />
+        <PersonalInfoForm
+          personalInfo={personalInfo}
+          handleChange={handlePersonalInfoChange}
+        />
         <EducationForm />
         <ExperienceForm />
       </aside>
+      <main>
+        <p>{personalInfo.firstName + personalInfo.lastName}</p>
+        <p>{personalInfo.position}</p>
+        <p>{personalInfo.email}</p>
+        <p>{personalInfo.phoneNumber}</p>
+        <p>{personalInfo.adress}</p>
+        <p>{personalInfo.summary}</p>
+      </main>
     </>
   );
 }
